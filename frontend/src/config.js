1d88889f8a095f3ad003a059a1015a9431765ab0
Atul Pathronia <atulpathronia@gmail.com>
2023-07-22 22:39:48 +0530

    changed samsaite and secure to none and true respectively

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,4 @@
 export const api = {
-  endpoint: "http://localhost:8000/api/v1",
+  // endpoint: "http://localhost:8000/api/v1",
+  endpoint: "https://urbaneman-backend.onrender.com/api/v1",
 };
--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -6,8 +6,8 @@ const createAndSendtoken = (user, statusCode, res) => {
       Date.now() + process.env.JWT_EXPIRESIN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    // sameSite: "None",
-    // secure: true,
+    sameSite: "None",
+    secure: true,
   };
 
   res.status(statusCode).cookie("token", token, options).json({
